2ac0fbc900f9d0a55480057192737404a655f1cc
Cameron Rohlfing <cameron.rohlfing@daugherty.com>
2019-02-06 12:59:33 -0600

    Brewery routes implemented

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const beerRouter = require('./routes/beerRouter');
+const breweryRouter = require('./routes/breweryRouter');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 const mongoose = require('mongoose');
-
-
 
 /////////////////////////////
 mongoose.connect('mongodb://localhost:27017/beers', { useNewUrlParser: true })
@@ -15,9 +14,16 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
    console.log(`Got an error!:\n${err}`);
 })
+mongoose.connect('mongodb://localhost:27017/breweries', { useNewUrlParser: true })
+mongoose.connection.on('connected', () => {
+   console.log('Connected to "breweries" database');
+})
+mongoose.connection.on('error', (err) => {
+   console.log(`Got an error!:\n${err}`);
+})
 
 app.use('/api/beers', beerRouter);
-
+app.use('/api/breweries', breweryRouter);
 
 app.use('/', (req, res) => {
   res.send('Hello!');
