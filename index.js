23e55c4cd8550a0e22e2cab14b8aa630028d8bdd
Cameron Rohlfing <cameron.rohlfing@daugherty.com>
2019-02-06 10:26:24 -0600

    Models created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const beerRouter = require('./routes/beerRouter');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+const mongoose = require('mongoose');
+
+
 
 /////////////////////////////
+mongoose.connect('mongodb://localhost:27017/beers', { useNewUrlParser: true })
+mongoose.connection.on('connected', () => {
+   console.log('Connected to "beers" database');
+})
+mongoose.connection.on('error', (err) => {
+   console.log(`Got an error!:\n${err}`);
+})
 
+app.use('/api/beers', beerRouter);
 
 
 app.use('/', (req, res) => {
